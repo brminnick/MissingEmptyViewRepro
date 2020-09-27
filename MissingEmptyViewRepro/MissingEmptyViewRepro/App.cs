76e1c3b1877e04f0191a4a8d1ba9e428f0d23d6d
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-09-27 11:01:59 -0700

    First Commit

---
+++ MissingEmptyViewRepro/MissingEmptyViewRepro/App.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace MissingEmptyViewRepro
+{
+    public class App : Application
+    {
+        public App() => MainPage = new ContentPage
+        {
+            Content = new CollectionView
+            {
+                EmptyView = new Image
+                {
+                    Source = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE2r0Th?ver=5b7d"
+                }
+            }
+        };
+    }
+}
